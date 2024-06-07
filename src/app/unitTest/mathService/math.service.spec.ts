ba2e2036d65f5d010d570fa569bfa0a337a8b14e
aymensellaouti <aymen.sellaouti@gmail.com>
2024-06-07 14:17:49 +0100

    unit test math service

--- src/app/unitTest/mathService/math.service.spec.ts
+++ src/app/unitTest/mathService/math.service.spec.ts
@@ -1,5 +1,27 @@
+import { LoggerService } from "src/app/services/logger.service";
+import { MathService } from "./math.service";
 
+let loggerService: LoggerService ;
+let mathService: MathService;
 
 fdescribe("MathService", () => {
-
+  beforeAll(() => {
+    // Arrange
+    loggerService = new LoggerService();
+    mathService = new MathService(loggerService);
+  });
+  it("should add two numbers", () => {
+    // Act
+    const result = mathService.add(2,3);
+    // Assert
+    const expectedResult = 5;
+    expect(result).toEqual(expectedResult);
+  })
+  it("should substract two numbers", () => {
+    // Act
+    const result = mathService.substract(2,3);
+    // Assert
+    const expectedResult = -1;
+    expect(result).toEqual(expectedResult);
+  })
 });
